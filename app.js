b6bc7107df6a77f8989e769d2d90a5808902843d
vlobarev <v.lobarev@elem.ru>
2016-07-26 14:19:40 +0500

    тестовый коммит

---
+++ app.js
@@ -0,0 +1,32 @@
+var koa = require('koa');
+var app = koa();
+
+// x-response-time
+
+app.use(function *(next){
+  var start = new Date;
+    console.log(next, this.method, this.url, ms);
+    yield next;
+    var ms = new Date - start;
+    this.set('X-Response-Time', ms + '1ms');
+    console.log(3, this.method, this.url, ms);
+});
+
+// logger
+
+app.use(function *(next){
+        var start = new Date;
+        console.log(2, this.method, this.url, ms);
+        yield next;
+        var ms = new Date - start;
+        console.log(4, this.method, this.url, ms);
+
+});
+
+// response
+
+app.use(function *(){
+  this.body = 'Hello World';
+});
+
+app.listen(3000);
