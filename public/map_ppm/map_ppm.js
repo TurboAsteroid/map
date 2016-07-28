f5438599a6a5e5038fc66d044ea6aa643c69c7f6
Nightwelf <nightwelf@gmail.com>
2016-07-28 10:16:18 +0500

    добавлена тестовая диаграмма

--- public/map_ppm/map_ppm.js
+++ public/map_ppm/map_ppm.js
@@ -5,7 +5,43 @@ angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
     .controller('map_svgController', function($scope, $http, $location, User) {
     })
     .controller('diagramController', function($scope, $http, $location, User) {
-        console.log(123222);
+        $(function () {
+            $('#ppm_diagram').highcharts({
+                chart: {
+                    type: 'bar'
+                },
+                title: {
+                    text: 'Stacked bar chart'
+                },
+                xAxis: {
+                    categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Total fruit consumption'
+                    }
+                },
+                legend: {
+                    reversed: true
+                },
+                plotOptions: {
+                    series: {
+                        stacking: 'normal'
+                    }
+                },
+                series: [{
+                    name: 'John',
+                    data: [5, 3, 4, 7, 2]
+                }, {
+                    name: 'Jane',
+                    data: [2, 2, 3, 2, 1]
+                }, {
+                    name: 'Joe',
+                    data: [3, 4, 4, 2, 5]
+                }]
+            });
+        });
     })
     .directive('ghVisualization', ['$location', function (location) {
         return {
