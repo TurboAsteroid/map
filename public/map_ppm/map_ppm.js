97f308554957ff821e7a69145ce1e6a4df14ad0f
Vasily Lobarev <vlobarev@gmail.com>
2016-07-29 13:52:21 +0500

    no reload

--- public/map_ppm/map_ppm.js
+++ public/map_ppm/map_ppm.js
@@ -1,69 +1,65 @@
 'use strict';
 angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
-    .controller('map_ppmController', function($scope, $http, $location, User) {
-    })
-    .controller('map_svgController', function($scope, $location, $element) {
-        // d3.select()
-
-
-
-
-
-
-
-
-        // $scope.clickonmap = function($event){
-        //     if ($event.target.tagName != "polygon") {
-        //         return;
-        //     }
-        //     $location.path('/map_ppm/'+angular.element($event.target).attr('id'));
-        // }
+    .controller('map_ppmController', function($scope, $http, $location, User, $routeParams, $rootScope) {})
+    .controller('map_svgController', function($scope, $location, $element, $rootScope) {
     })
     .controller('tableController', function($scope, $http, $location, User, $routeParams, $mdDialog) {
+        $scope.place = $routeParams.place || false;
+        $scope.$on('place_click', function (event, data) {
+            if ($scope.place != data.place) {
+                load_place(data.zone, data.place);
+            }
+        });
+
         if (!$routeParams.place) {
             return;
         }
-        $http({
-            method: 'POST',
-            url: '/api/get_table/',
-            data: {zone: $routeParams.zone, place: $routeParams.place}
-        }).then(function successCallback(response) {
-            $scope.table_name = response.data.place_name;
-            $scope.table_data = response.data.data;
-        }, function errorCallback(response) {
-            console.log('table request error');
-        });
 
-        $scope.items = {
-            incoming: {
-                name: "Приход/расход",
-                show: true
-            },
-            act: {
-                name: "Акт",
-                show: true
-            },
-            place: {
-                name: "место",
-                show: true
-            },
-            balance_at_start: {
-                name: "количестно на начало",
-                show: true
-            },
-            balance_at_start_OX: {
-                name: "количество на начало ОХ",
-                show: true
-            },
-            balance_at_start_work: {
-                name: "количество на начало в работу",
-                show: true
-            },
-            ei: {
-                name: "Единицы измерения",
-                show: true
-            }
-        };
+        load_place($routeParams.place, $routeParams.place);
+
+        function load_place (zone, place) {
+            $http({
+                method: 'POST',
+                url: '/api/get_table/',
+                data: {zone: zone, place: place}
+            }).then(function successCallback(response) {
+                $scope.table_name = response.data.place_name;
+                $scope.table_data = response.data.data;
+            }, function errorCallback(response) {
+                console.log('table request error');
+            });
+
+            $scope.items = {
+                incoming: {
+                    name: "Приход/расход",
+                    show: true
+                },
+                act: {
+                    name: "Акт",
+                    show: true
+                },
+                place: {
+                    name: "место",
+                    show: true
+                },
+                balance_at_start: {
+                    name: "количестно на начало",
+                    show: true
+                },
+                balance_at_start_OX: {
+                    name: "количество на начало ОХ",
+                    show: true
+                },
+                balance_at_start_work: {
+                    name: "количество на начало в работу",
+                    show: true
+                },
+                ei: {
+                    name: "Единицы измерения",
+                    show: true
+                }
+            };
+        }
 
         $scope.toggle = function (item) {
            item.show = item.show ? false : true;
@@ -79,110 +75,120 @@ angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
             });
         };
     })
-    .controller('diagramController', function($scope, $http, $location, User, $routeParams) {
+    .controller('diagramController', function($scope, $http, $location, User, $routeParams, $rootScope) {
         $scope.ppm_diagram_checkbox_show = false;
         $scope.ppm_diagram_show = true;
         $scope.ppm_diagram2_show = true;
 
+        $scope.zone = $routeParams.zone || false;
+        $scope.$on('zone_click', function (event, data) {
+            if ($scope.zone != data.zone) {
+                load_zone(data.zone);
+            }
+        });
         if (!$routeParams.zone) {
             return;
         }
-        $http({
-            method: 'POST',
-            url: '/api/get_diagram/',
-            data: {zone: $routeParams.zone}
-        }).then(function successCallback(response) {
-            $(function () {
-                $('#ppm_diagram').highcharts({
-                    chart: {
-                        type: 'bar'
-                    },
-                    title: {
-                        text: response.data.zone_name
-                    },
-                    xAxis: {
-                        categories: response.data.zones
-                    },
-                    yAxis: {
-                        min: 0,
-                        title: {
-                            text: 'Распределение сырья, тонны'
-                        }
-                    },
-                    // legend: {
-                    //     enabled: true
-                    // },
-                    plotOptions: {
-                        series: {
-                            stacking: 'normal',
-                            events: {
-                                click: function (event) {
-                                    $location.search("place="+event.point.category);
-                                    $scope.$apply();
 
+        load_zone($routeParams.zone);
+        function load_zone (zone) {
+            $scope.zone = zone;
+            $http({
+                method: 'POST',
+                url: '/api/get_diagram/',
+                data: {zone: zone}
+            }).then(function successCallback(response) {
+                $(function () {
+                    $('#ppm_diagram').highcharts({
+                        chart: {
+                            type: 'bar'
+                        },
+                        title: {
+                            text: response.data.zone_name
+                        },
+                        xAxis: {
+                            categories: response.data.zones
+                        },
+                        yAxis: {
+                            min: 0,
+                            title: {
+                                text: 'Распределение сырья, тонны'
+                            }
+                        },
+                        // legend: {
+                        //     enabled: true
+                        // },
+                        plotOptions: {
+                            series: {
+                                stacking: 'normal',
+                                events: {
+                                    click: function (event) {
+                                        history.pushState({}, event.point.category, '#'+$location.path()+'?place='+event.point.category);
+                                        $rootScope.$broadcast("place_click", {zone: zone, place: event.point.category});
+                                    }
                                 }
                             }
-                        }
-                    },
-                    series: response.data.data
+                        },
+                        series: response.data.data
+                    });
                 });
+                $scope.ppm_diagram_checkbox_show = true;
+            }, function errorCallback(response) {
+                console.log('diagram request error');
             });
-            $scope.ppm_diagram_checkbox_show = true;
-        }, function errorCallback(response) {
-            console.log('diagram request error');
-        });
 
-        $http({
-            method: 'POST',
-            url: '/api/get_diagram2/',
-            data: {zone: $routeParams.zone}
-        }).then(function successCallback(response) {
-            $(function () {
-                $('#ppm_diagram2').highcharts({
-                    chart: {
-                        type: 'bar'
-                    },
-                    title: {
-                        text: response.data.zone_name
-                    },
-                    xAxis: {
-                        categories: response.data.zones
-                    },
-                    yAxis: {
-                        min: 0,
+            $http({
+                method: 'POST',
+                url: '/api/get_diagram2/',
+                data: {zone: zone}
+            }).then(function successCallback(response) {
+                $(function () {
+                    $('#ppm_diagram2').highcharts({
+                        chart: {
+                            type: 'bar'
+                        },
                         title: {
-                            text: 'Распределение сырья, проценты'
-                        }
-                    },
-                    legend: {
-                        enabled: true
-                    },
-                    plotOptions: {
-                        series: {
-                            stacking: 'normal'
-                        }
-                    },
-                    series: response.data.data
+                            text: response.data.zone_name
+                        },
+                        xAxis: {
+                            categories: response.data.zones
+                        },
+                        yAxis: {
+                            min: 0,
+                            title: {
+                                text: 'Распределение сырья, проценты'
+                            }
+                        },
+                        legend: {
+                            enabled: true
+                        },
+                        plotOptions: {
+                            series: {
+                                stacking: 'normal'
+                            }
+                        },
+                        series: response.data.data
+                    });
                 });
+                $scope.ppm_diagram_checkbox_show = true;
+            }, function errorCallback(response) {
+                console.log('diagram request error');
             });
-            $scope.ppm_diagram_checkbox_show = true;
-        }, function errorCallback(response) {
-            console.log('diagram request error');
-        });
+        }
     })
-    .directive('ghVisualization', ['$location', function (location) {
+    .directive('ghVisualization', function ($location, $rootScope) {
         return {
             restrict: 'E', // the directive can be invoked only by using <my-directive> tag in the template
             link: function (scope, element, attrs) {
                 var parent = d3.select(element[0]);
                 var polygon = parent.selectAll('polygon');
                 polygon.on("click", function(){
-                    location.path('/map_ppm/'+d3.select(this).attr('id'));
-                    scope.$apply();
+                    history.pushState({}, d3.select(this).attr('id'), '#/map_ppm/'+d3.select(this).attr('id'));
+                    $rootScope.$broadcast("zone_click", {zone:d3.select(this).attr('id')});
                 });
             }
         }
-    }]);
+    });
 
 
 function DialogController($scope, $mdDialog) {
