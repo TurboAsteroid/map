85cc6174cbe053dad49d0ab81cc639f119c86e42
Vasily Lobarev <vlobarev@gmail.com>
2016-08-01 10:45:03 +0500

    zoom, remove location api

--- public/map_ppm/map_ppm.js
+++ public/map_ppm/map_ppm.js
@@ -10,46 +10,38 @@ angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
         });
     })
     .controller('tableController', function($scope, $http, $location, User, $routeParams, $mdDialog, localStorageService) {
-        $scope.place = $routeParams.place || false;
         $scope.$on('place_click', function (event, data) {
-            if ($scope.place != data.place) {
-                load_place(data.zone, data.place);
-            }
+            load_place(data.zone, data.place, data.raw);
         });
+        $scope.items = {
+            incoming: "Приход/расход",
+            act: "Акт",
+            place: "место",
+            balance_at_start: "количестно на начало",
+            balance_at_start_OX: "количество на начало ОХ",
+            balance_at_start_work: "количество на начало в работу",
+            ei: "Единицы измерения"
+        };
 
-        if (!$routeParams.place) {
-            return;
-        }
-
-        load_place($routeParams.place, $routeParams.place);
-
-        function load_place (zone, place) {
+        function load_place (zone, place, raw) {
             $http({
                 method: 'POST',
                 url: '/api/get_table/',
-                data: {zone: zone, place: place}
+                data: {zone: zone, place: place, raw: raw}
             }).then(function successCallback(response) {
                 $scope.table_name = response.data.place_name;
                 $scope.table_data = response.data.data;
             }, function errorCallback(response) {
                 console.log('table request error');
             });
-
-            $scope.items = {
-                incoming: "Приход/расход",
-                act: "Акт",
-                place: "место",
-                balance_at_start: "количестно на начало",
-                balance_at_start_OX: "количество на начало ОХ",
-                balance_at_start_work: "количество на начало в работу",
-                ei: "Единицы измерения"
-            };
         }
 
         $scope.toggle = function (key) {
             localStorageService.set(key, $scope.checkVisible(key) ? 'false' : 'true');
         };
         $scope.checkVisible = function (key) {
+
+            console.log(key, localStorageService.get(key));
             return localStorageService.get(key) === 'false' ? false : true;
         };
 
@@ -66,18 +58,11 @@ angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
     .controller('diagramController', function($scope, $http, $location, User, $routeParams, $rootScope) {
         $scope.ppm_diagram_checkbox_show = false;
         $scope.ppm_diagram_show = true;
-        $scope.ppm_diagram2_show = true;
+        $scope.ppm_diagram2_show = false;
 
-        $scope.zone = $routeParams.zone || false;
         $scope.$on('zone_click', function (event, data) {
-            if (data.zone!=$scope.zone) {
-                load_zone(data.zone);
-            }
+            load_zone(data.zone);
         });
-        if (!$routeParams.zone) {
-            return;
-        }
-        load_zone($routeParams.zone);
 
         function load_zone (zone) {
             $scope.zone = zone;
@@ -106,15 +91,12 @@ angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
                                 text: 'Распределение сырья, тонны'
                             }
                         },
-                        // legend: {
-                        //     enabled: true
-                        // },
                         plotOptions: {
                             series: {
                                 stacking: 'normal',
                                 events: {
                                     click: function (event) {
-                                        history.pushState({}, event.point.category, '#'+$location.path()+'?place='+event.point.category);
+                                        // history.pushState({}, event.point.category, '#'+$location.path()+'?place='+event.point.category);
                                         $rootScope.$broadcast("place_click", {zone: zone, place: event.point.category});
                                     }
                                 }
@@ -140,7 +122,13 @@ angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
                         },
                         plotOptions: {
                             series: {
-                                stacking: 'normal'
+                                stacking: 'normal',
+                                events: {
+                                    click: function (event) {
+                                        // history.pushState({}, event.point.category, '#'+$location.path()+'?raw='+event.point.category);
+                                        $rootScope.$broadcast("place_click", {zone: zone, raw: event.point.category});
+                                    }
+                                }
                             }
                         },
                         series: response.data.data_raws
@@ -184,7 +172,7 @@ angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
                                 stacking: 'normal',
                                 events: {
                                     click: function (event) {
-                                        history.pushState({}, event.point.category, '#'+$location.path()+'?place='+event.point.category);
+                                        // history.pushState({}, event.point.category, '#'+$location.path()+'?place='+event.point.category);
                                         $rootScope.$broadcast("place_click", {zone: zone, place: event.point.category});
                                     }
                                 }
@@ -217,6 +205,13 @@ angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
                     .style("background", "#fff")
                     .style("border", "1px solid #ccc")
                     .style("border-radius", "7px");
+
+                var svg_group = parent.select('g');
+                parent.select("svg").call(d3.zoom().on("zoom", zoomed));
+                function zoomed() {
+                    svg_group.attr("transform", d3.event.transform);
+                }
+
                 polygon
                     .on("mouseover", function(){
                         tooltip.text(this.id);
@@ -230,7 +225,7 @@ angular.module('map_ppmController', ['ngRoute', 'ngMaterial'])
                     })
                     .on("click", function(){
                         tooltip.style("visibility", "hidden");
-                        history.pushState({}, d3.select(this).attr('id'), '#/map_ppm/'+d3.select(this).attr('id'));
+                        // history.pushState({}, d3.select(this).attr('id'), '#/map_ppm/'+d3.select(this).attr('id'));
                         $rootScope.$broadcast("zone_click", {zone:d3.select(this).attr('id')});
                     });
             }
