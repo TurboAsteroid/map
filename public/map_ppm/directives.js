1cb0b807b040538bcb41153c55f0309613da145d
vlobarev <v.lobarev@elem.ru>
2016-08-05 17:16:18 +0500

    test zoom

--- public/map_ppm/directives.js
+++ public/map_ppm/directives.js
@@ -51,7 +51,6 @@ angular.module('directivesModule', [])
                             tooltip.hide();
                         })
                         .on("mousemove", function(event){
-                            console.log(123);
                             tooltip.css({
                                 "top": (event.pageY - 10) + "px",
                                 "left": (event.pageX + 10) + "px"
@@ -68,8 +67,7 @@ angular.module('directivesModule', [])
                 var svg_group = parent.select('g');
                 Zoom.set({zoom_obj: d3.zoom()});
                 var tmp_transform = {x: 0, y: 0, k: 1};
-                Zoom.get('zoom_obj').scaleExtent([0.7, 6]);
-                // Zoom.get('zoom_obj').translateExtent([[0, 0], [3000, 4000]]);
+                Zoom.get('zoom_obj').scaleExtent([1, 6]);
                 parent.select("svg").call(Zoom.get('zoom_obj').on("zoom", function() {
                     Zoom.set({transform: d3.event.transform});
                     svg_group.attr("transform", d3.event.transform);
--- public/map_ppm/map_svg/map_svg.js
+++ public/map_ppm/map_svg/map_svg.js
@@ -16,7 +16,7 @@ angular.module('map_svgModule', ['ngRoute'])
             $scope.showLegend = localStorageService.get("showLegend");
             $scope.action_legend_name = $scope.legend_name();
         };
-        $scope.legend_name = function($event,area_id) {
+        $scope.legend_name = function() {
             return localStorageService.get("showLegend") === true ? "Скрыть зоны ответственности" : "Показать зоны ответственности"
         };
         $scope.show_legend_items = function($event,area_id) {
@@ -26,17 +26,21 @@ angular.module('map_svgModule', ['ngRoute'])
         $scope.getWidth = function () {
             return $($element).width();
         };
-        $('#svg_map').height($('#svg_map').width());
+
+        var map_block = $('#svg_map');
+        map_block.height(map_block.width());
+        var center = [map_block.width()/2, map_block.height() / 2];
+
         $(window).resize(function(){
-            $('#svg_map').height($('#svg_map').width());
+            map_block.height(map_block.width());
+            center = [map_block.width()/2, map_block.height() / 2];
         });
 
-
         $scope.zoom_map = function (new_zoom) {
-            var transform = Zoom.get('transform');
-            transform = {
-                x : transform.x,
-                y : transform.y,
+            var old_transform = Zoom.get('transform');
+            var new_transform = {
+                x : center[0] - (center[0]*new_zoom / old_transform.k),
+                y : center[1] - (center[1]*new_zoom / old_transform.k),
                 k : new_zoom
             };
 
@@ -44,9 +48,10 @@ angular.module('map_svgModule', ['ngRoute'])
             if (!Zoom.get('zoom_obj')) {
                 Zoom.set({zoom_obj: d3.zoom()});
             }
-            Zoom.get('zoom_obj').scaleTo(elem, transform.k);
-            Zoom.set({transform: transform});
-        }
+            Zoom.get('zoom_obj').scaleTo(elem, new_transform.k);
+            Zoom.get('zoom_obj').translateBy(elem, new_transform.x, new_transform.y);
+            Zoom.set({transform: new_transform});
+        };
 
         $scope.zoom_lvl = 1;
 
@@ -55,9 +60,4 @@ angular.module('map_svgModule', ['ngRoute'])
         }, function(zoom) {
             $scope.zoom_map(zoom);
         });
-        // $scope.$watch(function(){
-        //     return Zoom.get('transform').k;
-        // }, function(zoom) {
-        //     $scope.zoom_lvl = parseFloat(zoom);
-        // });
     });
