ec5e8f645168ec46df8f0b44ede4979b71545645
Vasily Lobarev <vlobarev@gmail.com>
2016-08-11 11:41:57 +0500

    Timeline - test data

--- public/map_ppm/map_ppm.js
+++ public/map_ppm/map_ppm.js
@@ -13,6 +13,7 @@ angular.module('map_ppmModule', ['ngRoute', 'ngMaterial', 'directivesModule', 'f
         var raw = $location.search().raw;
         var zone = $location.search().svg_zone;
         $rootScope.table = raw || place;
+        $rootScope.raw = raw;
         $rootScope.zone = zone;
 
         $rootScope.$on('$locationChangeSuccess', function(event) {
@@ -20,6 +21,29 @@ angular.module('map_ppmModule', ['ngRoute', 'ngMaterial', 'directivesModule', 'f
             var raw = $location.search().raw;
             var zone = $location.search().svg_zone;
             $rootScope.table = raw || place;
+            $rootScope.raw = raw;
             $rootScope.zone = zone;
+        });
+
+
+        Highcharts.setOptions({
+            lang: {
+                loading: 'Загрузка...',
+                months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                weekdays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+                shortMonths: ['Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сент', 'Окт', 'Нояб', 'Дек'],
+                exportButtonTitle: "Экспорт",
+                printButtonTitle: "Печать",
+                rangeSelectorFrom: "С",
+                rangeSelectorTo: "По",
+                rangeSelectorZoom: "Период",
+                downloadPNG: 'Скачать PNG',
+                downloadJPEG: 'Скачать JPEG',
+                downloadPDF: 'Скачать PDF',
+                downloadSVG: 'Скачать SVG',
+                printChart: 'Напечатать график',
+                resetZoom: 'Сбросить увеличение',
+                drillUpText: 'Назад к {series.name}'
+            }
         });
     });
