9a98b5ba3c08bab7d050db7ce16d71d851600a34
Vasily Lobarev <vlobarev@gmail.com>
2016-09-05 14:08:33 +0500

    правки на сервере в конфиге

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	//порты сервера ноды
 	'portHttp': '7070',
 	//сервер  АД
-	'adServer': 'ldap://elem-dc0.elem.ru',
+	'adServer': 'ldap://elem-dc1.elem.ru',
 	//домен
 	'adBaseDN': 'dc=elem,dc=ru',
 	//пользователь АД, имеющий доступ к АД. требуется для простого получения списка пользоватлей АД
@@ -10,7 +10,7 @@ module.exports = {
 	//пароль пользователя АД
 	'adPassword': 'gs2-1',
 	'cookieSecret': '03836880c1e13cd15430a9b3fc6f04a5eb2ee12330148ac6695813ca73f1880c7d15b671d7dbb57e10e74de719ce1b66905042446a12be04981807194391bcef',
-	'dbHost' : '10.1.255.191',
+	'dbHost' : '10.1.255.117',
 	'dbDatabase' : 'ppm-map',
 	'dbUser': 'dev_db',
 	'dbPassword': 'dev_me_please',
